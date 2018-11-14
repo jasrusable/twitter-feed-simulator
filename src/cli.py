65cb61c19d0d4e6ee25cb258854f53c514a20587
Jason Russell <jasrusable@gmail.com>
2018-11-14 21:17:19 +0200

    Add file params to root command

--- src/cli.py
+++ src/cli.py
@@ -7,18 +7,24 @@ from src.users.lib import build_user_follower_mapping_for_multiple_follow_events
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--tweets-file', help='Path for file containing tweets.')
+@click.option('--user-mapping-file', help='Path for file containing user follower mapping.')
+@click.pass_context
+def cli(ctx, tweets_file, user_mapping_file):
+    ctx.ensure_object(dict)
+    ctx.obj['tweets_file'] = tweets_file
+    ctx.obj['user_mapping_file'] = user_mapping_file
 
 
 @click.command()
-@click.option('--tweets-file', help='Path for file containing tweets.')
-@click.option('--user-mapping-file', help='Path for file containing user follower mapping.')
-def print_users_feeds(tweets_file, user_mapping_file):
-    follow_events = parse_follow_event_file('sample_data/events.txt')
+@click.pass_context
+def print_users_feeds(ctx):
+    user_mapping_file = ctx.obj['user_mapping_file']
+    tweets_file = ctx.obj['tweets_file']
+    follow_events = parse_follow_event_file(user_mapping_file)
     user_follower_mapping = build_user_follower_mapping_for_multiple_follow_events(follow_events)
     users = sorted(get_users_from_user_follower_mapping(user_follower_mapping))
-    tweets = parse_tweet_file('sample_data/tweets.txt')
+    tweets = parse_tweet_file(tweets_file)
     output = get_user_feeds_output(users, user_follower_mapping, tweets)
     for line in output:
         print(line)

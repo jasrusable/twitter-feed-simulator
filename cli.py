6631b5da55412f129f62573a4407584c903fa767
Jason Russell <jasrusable@gmail.com>
2018-11-15 11:21:53 +0200

    Rename events and user_mapping to follow_events

--- cli.py
+++ cli.py
@@ -8,23 +8,23 @@ from src.users.lib import build_user_follower_mapping_for_multiple_follow_events
 
 @click.group()
 @click.option('--tweets-file', help='Path for file containing tweets.')
-@click.option('--user-mapping-file', help='Path for file containing user follower mapping.')
+@click.option('--follow-events-file', help='Path for file containing follow events.')
 @click.pass_context
-def cli(ctx, tweets_file, user_mapping_file):
+def cli(ctx, tweets_file, follow_events_file):
     ctx.ensure_object(dict)
     ctx.obj['tweets_file'] = tweets_file
-    ctx.obj['user_mapping_file'] = user_mapping_file
+    ctx.obj['follow_events_file'] = follow_events_file
 
 
 @click.command()
 @click.pass_context
 def print_users_feeds(ctx):
-    user_mapping_file = ctx.obj['user_mapping_file']
-    tweets_file = ctx.obj['tweets_file']
-    follow_events = parse_follow_event_file(user_mapping_file)
+    follow_events_file_path = ctx.obj['follow_events_file']
+    tweets_file_path = ctx.obj['tweets_file']
+    follow_events = parse_follow_event_file(follow_events_file_path)
     user_follower_mapping = build_user_follower_mapping_for_multiple_follow_events(follow_events)
     users = sorted(get_users_from_user_follower_mapping(user_follower_mapping))
-    tweets = parse_tweet_file(tweets_file, line_cb=validate_tweet_line, tweet_cb=validate_tweet)
+    tweets = parse_tweet_file(tweets_file_path, line_cb=validate_tweet_line, tweet_cb=validate_tweet)
     output = get_user_feeds_output(users, user_follower_mapping, tweets)
     for line in output:
         print(line)

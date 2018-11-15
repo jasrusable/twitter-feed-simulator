bd2beab2d9ac65470b462dbc67d226c5e1c776d0
Jason Russell <jasrusable@gmail.com>
2018-11-15 13:27:21 +0200

    Restructure code

--- src/tweets/lib.py
+++ src/tweets/lib.py
@@ -4,6 +4,19 @@ from collections import namedtuple
 Tweet = namedtuple('Tweet', ['author', 'body'])
 
 
+def validate_tweet(tweet, meta):
+    reasons = get_reasons_tweet_invalid(tweet)
+    if reasons:
+        raise Exception(f'{reasons[0]} meta: {str(meta)}')
+
+
+def get_reasons_tweet_invalid(tweet):
+    reasons = []
+    if len(tweet['body']) > 140:
+        reasons.append(f'Tweet length exceeds 140 characters.')
+    return reasons or None
+
+
 # TODO: Use generator?
 # TODO: Rename to filter_tweets?
 def get_user_tweet_feed(user, followers, tweets):
@@ -17,16 +30,3 @@ def get_user_tweet_feed(user, followers, tweets):
         if author == user or author in followers:
             feed.append(tweet)
     return feed
-
-
-def get_reasons_tweet_invalid(tweet):
-    reasons = []
-    if len(tweet['body']) > 140:
-        reasons.append(f'Tweet length exceeds 140 characters.')
-    return reasons or None
-
-
-def validate_tweet(tweet, meta):
-    reasons = get_reasons_tweet_invalid(tweet)
-    if reasons:
-        raise Exception(f'{reasons[0]} meta: {str(meta)}')
